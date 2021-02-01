e9842c457852c6e51ed833dc8084f7bc3194a115
HeidariHamed <hamed.heydari62@gmail.com>
2021-02-02 00:15:58 +0900

    changed biuld.gradle file for Configuration integration test.

--- src/main/java/com/game/dto/PlayerDto.java
+++ src/main/java/com/game/dto/PlayerDto.java
@@ -2,12 +2,14 @@ package com.game.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PlayerDto {
     @JsonFormat(pattern="HH:mm:ss")
     private LocalDateTime time;
